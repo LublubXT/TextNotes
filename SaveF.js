07703ca3e6a011dcc515e5a487dbc4c61b779234
LublubXT <73581388+LublubXT@users.noreply.github.com>
2021-01-25 13:05:00 +0200

    Added files

--- SaveF.js
+++ SaveF.js
@@ -62,6 +62,49 @@ function save() {
     document.getElementById('download').setAttribute('download', fileName);
 }
 
+document.getElementById('btnOpen').onclick = function () {
+    openFile(function (txt) {
+        document.getElementById('texta').innerText = txt;
+    });
+}
+
+function openFile(callBack) {
+    var element = document.createElement('input');
+    element.setAttribute('type', "file");
+    element.setAttribute('id', "btnOpenFile");
+    element.onchange = function () {
+        readText(this, callBack);
+        document.body.removeChild(this);
+    }
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+}
+
+function readText(filePath, callBack) {
+    var reader;
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        reader = new FileReader();
+    } else {
+        alert('The File APIs are not fully supported by your browser. Fallback required.');
+        return false;
+    }
+    var output = ""; //placeholder for text output
+    if (filePath.files && filePath.files[0]) {
+        reader.onload = function (e) {
+            output = e.target.result;
+            callBack(output);
+        };//end onload()
+        reader.readAsText(filePath.files[0]);
+    }//end if html5 filelist support
+    else { //this is where you could fallback to Java Applet, Flash or similar
+        return false;
+    }
+    return true;
+}
+
 // var x = document.getElementById('filename');
 
 // var fileName = x.value + '.txt';
